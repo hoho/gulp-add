0023b436daa8c63cf3b035f3b51775658e00476b
Cody Taylor <codemister99@yahoo.com>
2018-04-17 10:58:25 -0400

    Allow stream to pass through existing files.
    Remove buffer import, the value `Buffer` is global in node.
    Replace `new Buffer` with `Buffer.from`.
    Replace `contents instanceof Buffer` with `Buffer.isBuffer`.

--- index.js
+++ index.js
@@ -8,35 +8,30 @@
 var through = require('through2');
 var Vinyl = require('vinyl');
 var PluginError = require('plugin-error');
-var Buffer = require('buffer').Buffer;
 
 var PLUGIN_NAME = 'gulp-add';
 
 function createFile(path, content) {
     return new Vinyl({
         path: path,
-        contents: content
+        contents: Buffer.isBuffer(content) ? content : Buffer.from(content)
     });
 }
 
 module.exports = function(files, content, before) {
-    var oldFiles = [];
-    var newFiles = [];
+    var addFiles = [];
 
     if(typeof files === 'object') {
         for(var file in files) {
             if(files.hasOwnProperty(file)) {
-                newFiles.push(createFile(file, new Buffer(files[file])));
+                addFiles.push(createFile(file, files[file]));
             }
         }
         before = content;
     }
     else if(typeof files === 'string') {
-        if(typeof content === 'string') {
-            newFiles.push(createFile(files, new Buffer(content)));
-        }
-        else if(typeof content === 'object' && content instanceof Buffer) {
-            newFiles.push(createFile(files, content));
+        if(typeof content === 'string' || Buffer.isBuffer(content)) {
+            addFiles.push(createFile(files, content));
         }
         else {
             throw new PluginError(PLUGIN_NAME, 'Unknown argument type.');
@@ -52,36 +47,26 @@ module.exports = function(files, content, before) {
 
     return through.obj(
         function(file, encoding, callback) {
-            if(file.isNull()) {
-                return callback(null, file);
+            if(before) {
+                pushAll(this, addFiles);
+                before = null;
             }
 
             if(file.isStream()) {
                 return callback(new PluginError(PLUGIN_NAME, 'Streaming is not supported.'));
             }
 
-            oldFiles.push(file);
-
-            return callback(null);
+            return callback(null, file);
         },
         function(callback) {
-            try {
-                var startFiles = before ? newFiles : oldFiles;
-                var endFiles = before ? oldFiles : newFiles;
-
-                for(var i = 0; i < startFiles.length; i++) {
-                    this.push(startFiles[i]);
-                }
-
-                for(var i = 0; i < endFiles.length; i++) {
-                    this.push(endFiles[i]);
-                }
-
-                return callback();
-            }
-            catch(error) {
-                return callback(new PluginError(PLUGIN_NAME, error.message));
-            }
+            pushAll(this, addFiles);
+            callback(null);
         }
     );
 };
+
+function pushAll(stream, files) {
+    while (files && files.length) {
+        stream.push(files.shift());
+    }
+}
